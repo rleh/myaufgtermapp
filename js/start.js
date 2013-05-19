a104db34f8a569063d73a446747643d604bb4e3c
Raphael Lehmann <rll.lehmann@gmail.com>
2013-05-19 16:31:01 +0200

    bug in a html tag

--- js/start.js
+++ js/start.js
@@ -1,9 +1,10 @@
- 
-$("#start_aufgterm").click( function() {
-	location.href='aufgterm.html';
+$("#start_b_aufgterm").click( function() {
+	alert("aufgterm!!");
+	location.href="aufgterm.html";
 });
 
 
-$("#start_login").click( function() {
-	location.href='login.html';
+$("#start_b_login").click( function() {
+	location.href="login.html";
 });
+

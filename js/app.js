a85065c25c386f79816e96a00ce75e80f63a0d14
Raphael Lehmann <rll.lehmann@gmail.com>
2013-05-20 13:41:13 +0200

    notification and other improvements ...

--- js/app.js
+++ js/app.js
@@ -7,7 +7,36 @@ function my_back() {
 }
 
 function my_logout() {
-	alert("mylogout");
+	window.localStorage.removeItem("my_user");
+	window.localStorage.removeItem("my_pw");
+	location.href='index.html';
+}
+
+function t_goto() {
+	location.href="aufgterm.html";
+}
+
+function t_checknew() {
+	var my_pw = window.localStorage.getItem("my_pw");
+	var my_user = window.localStorage.getItem("my_user");
+	
+	if(window.localStorage.getItem("my_user") != null || window.localStorage.getItem("my_pw") != null) {
+		var my_pw = window.localStorage.getItem("my_pw");
+		var my_user = window.localStorage.getItem("my_user");
+		var oldtimestamp = window.localStorage.getItem("t_timestamp");
+		$.post("http://app.mytfg.de/app/php/t_checknew.php", { user: my_user, pw: my_pw })
+			.done(function(data) {
+				if(data != oldtimestamp || oldtimestamp != null || data != null){
+					window.localStorage.setItem("t_timestamp", data);
+					navigator.notification.alert("Neue Aufgabe", t_goto(),"MyTFG Aufgaben Terminal", "Anzeigen");
+					navigator.notification.vibrate(milliseconds);
+				}
+			});
+	}
+//	else {
+//		 Nix
+//	}
+
 }
 
 
@@ -19,10 +48,11 @@ $(".header_back").click( function() {
 	my_back();
 });
 
-$("#my_logout").click( function() {
+$(".my_logout").click( function() {
 	my_logout();
 });
 
-$("#my_settings").click( function() {
+$(".my_settings").click( function() {
 	location.href='settings.html';
 });
+
--- js/login.js
+++ js/login.js
@@ -1,18 +1,28 @@
+$(document).ready(function() {
+	if(window.localStorage.getItem("my_user") != null) {
+		$("#f_user").val( window.localStorage.getItem("my_user") );
+	}
+	if(window.localStorage.getItem("my_pw") != null) {
+		$("#f_pw").val( window.localStorage.getItem("my_pw") );
+	}
+});
+
 $("#b_login").click(function() {
-  $.get("http://app.mytfg.de/app/php/login.php", { user: $("#f_user").val(), pw: $("#f_pw").val() })
-	.done(function(data) {
-		if(data=="login_ok"){
-			alert("Anmeldung erfolgreich!");
-			
-			// Anmeldedaten Abspeichern!
-			
-			location.href='aufgterm.html';
-		}
-		else if(answer=="login_incorrect"){
-			alert("Anmeldedaten falsch!");
-		}
-		else{
-			alert("Anmeldung fehlgeschlagen!");
-		}
-	});
+	var my_pw = $("#f_pw").val();
+	var my_user = $("#f_user").val();
+	$.post("http://app.mytfg.de/app/php/login.php", { user: my_user, pw: my_pw })
+		.done(function(data) {
+			if(data=="login_ok"){
+				window.localStorage.setItem("my_user", my_user);
+				window.localStorage.setItem("my_pw", my_pw);
+				alert("Anmeldung erfolgreich!");
+				location.href='aufgterm.html';
+			}
+			else if(answer=="login_incorrect"){
+				alert("Anmeldedaten falsch!");
+			}
+			else{
+				alert("Anmeldung fehlgeschlagen!");
+			}
+		});
 });
